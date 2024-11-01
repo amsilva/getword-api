e4b7071499a8098af83d03068249f1466897fcb3
andre <andre.marcos@gmail.com>
2024-11-01 04:30:15 -0300

    base ok api

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 from flask import Flask, jsonify
+import csv
+import random
+
+arquivo_txt = "dicionario.txt"
 
 app = Flask(__name__)
 
-@app.route('/get', methods=['GET'])
-def get_message():
-    return jsonify(palavra="GOIABA")
+def open_dicionario(arquivo_txt, c):
+    dados = []
+    with open(arquivo_txt, mode='r') as meuarquivo:
+        buffer = csv.reader(meuarquivo)
+        for linha in buffer:
+            print(linha)
+            if  linha[0] == str(c) :
+                complex = linha[0]
+                cat = linha[1]
+                pal = linha[2]
+                dados.append({'complexidade': complex, 'categoria': cat, 'palavra':pal})
+    return dados
+
+def sortword(cat) :
+    dicionario = open_dicionario(arquivo_txt, 1) ##dicionario
+    ##palavra_sorteada = dicionario[0]
+    pos = random.randint(0,len(dicionario)-1)
+    palavra_sorteada = dicionario[pos]
+    ##palavra_sorteada = dicionario[random.randint(0,len(dicionario)-1)]
+    return palavra_sorteada
+
+
+@app.route('/simple-get', methods=['GET'])
+def get_simple_message():
+    novapalavra = sortword(1)
+    return jsonify(palavra=novapalavra['palavra'])
+
+@app.route('/complex-get', methods=['GET'])
+def get_complex_message():
+    novapalavra = sortword(1)
+    return jsonify(palavra=novapalavra['palavra'], 
+                categoria=novapalavra['categoria'],
+                complexidade=novapalavra['complexidade'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
