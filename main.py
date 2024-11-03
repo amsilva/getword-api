52998b0c8e2ef15009dc8cdf3192cb1d7a480a17
andre <andre.marcos@gmail.com>
2024-11-03 04:59:17 -0300

    teste deploy no webapp ok

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 from flask import Flask, jsonify
 import csv
 import random
+from bs4 import BeautifulSoup
+import requests
 
 arquivo_txt = "dicionario.txt"
+url_txt = "https://dontpad.com/faccamphoy/getworld/dicionario"
 
 app = Flask(__name__)
 
-def open_dicionario(arquivo_txt, c):
+def open_webdicionario():
+    # URL da página Don'tPad
+    url = url_txt
+
+    # Faz uma requisição para obter o conteúdo da página
+    response = requests.get(url)
+
+    # Verifica se a requisição foi bem-sucedida
+    if response.status_code == 200:
+        # Faz o parsing do conteúdo HTML
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Encontra o conteúdo na página
+        conteudo = soup.get_text()
+
+        # Exibe o conteúdo
+        print(conteudo)
+    else:
+        print("Erro ao acessar a página:", response.status_code)
+
+def open_localdicionario(arquivo_txt, c):
     dados = []
     with open(arquivo_txt, mode='r') as meuarquivo:
         buffer = csv.reader(meuarquivo)
         for linha in buffer:
-            print(linha)
+            ## print(linha) #amsdebug
             if  linha[0] == str(c) :
                 complex = linha[0]
                 cat = linha[1]
@@ -20,7 +43,8 @@ def open_dicionario(arquivo_txt, c):
     return dados
 
 def sortword(cat) :
-    dicionario = open_dicionario(arquivo_txt, 1) ##dicionario
+    open_webdicionario()
+    dicionario = open_localdicionario(arquivo_txt, 1) ##dicionario
     ##palavra_sorteada = dicionario[0]
     pos = random.randint(0,len(dicionario)-1)
     palavra_sorteada = dicionario[pos]
